1163300321b7a671b4d5b191bd7e98c7eb186e60
Kade Quakenbush <jedimasterkade@gmail.com>
2025-03-19 12:57:50 +0000

    Added lemmatisation

---
+++ bug_report_classification/src/utils.py
@@ -0,0 +1,78 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('stopwords')
+nltk.download('punkt_tab')
+nltk.download('wordnet')
+
+debug_mode = False
+
+def debug(str):
+    if debug_mode:
+        print("[DEBUG]", str)
+
+# Define text preprocessing methods
+def remove_html(text):
+    """Remove HTML tags using a regex."""
+    html = re.compile(r'<.*?>')
+    return html.sub(r'', text)
+
+def remove_emoji(text):
+    """Remove emojis using a regex pattern."""
+    emoji_pattern = re.compile("[" 
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"  # enclosed characters
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
+
+# Stopwords
+NLTK_stop_words_list = stopwords.words('english')
+custom_stop_words_list = ['...']  # You can customize this list as needed
+final_stop_words_list = NLTK_stop_words_list + custom_stop_words_list
+
+def remove_stopwords(text):
+    """Remove stopwords from the text."""
+    return " ".join([word for word in str(text).split() if word not in final_stop_words_list])
+
+def clean(string):
+    """
+    Clean text by removing non-alphanumeric characters,
+    and convert it to lowercase.
+    """
+    string = re.sub(r"[^A-Za-z0-9(),.!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"\)", " ) ", string)
+    string = re.sub(r"\?", " ? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    string = re.sub(r"\\", "", string)
+    string = re.sub(r"\'", "", string)
+    string = re.sub(r"\"", "", string)
+    return string.strip().lower()
+
+# Lemmatiser
+lemmatizer = WordNetLemmatizer()
+
+def lemmatize(text):
+    # Tokenise
+    tokens = word_tokenize(text)
+    tokens = [word for word in tokens if word not in NLTK_stop_words_list]
+    # Lemmatise
+    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    text = " ".join(tokens)
+    return text
+
+def pre_process(text):
+    text = remove_html(text)
+    text = remove_emoji(text)
+    text = remove_stopwords(text)
+    text = clean(text)
+    text = lemmatize(text)
+    return text
